d5dccc443898ced090964ba114fd0291775ae1bb
Levent Ince <levent.ince@shopify.com>
2019-12-16 09:22:41 -0500

    add validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,7 @@ class Item < ApplicationRecord
   has_many :item_variants
   has_many :variants, through: :item_variants
   has_many :inventory_items , dependent: :destroy
+
+  validates :name, presence: true, length: { minimum: 3 }, uniqueness: { case_sensitive: true
+  validates :quantity_threshold,  numericality: { only_integer: true }
 end
